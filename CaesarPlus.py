efc7efb16e629221960adfe48e3e7d96a9966e98
bluend <119117334+bluend@users.noreply.github.com>
2024-05-30 21:57:39 +0900

    Update CaesarPlus.py
    Encryption update

--- CaesarPlus.py
+++ CaesarPlus.py
@@ -4,19 +4,39 @@
 # It use ex ) CaesarCode( ~~~ )
 
 
-from time import time #시저 플러스 암호화 만드는데 걸리는 시간 세는거
+from time import time #counting time
 from sys import setrecursionlimit
 from random import shuffle, randrange
 from string import digits, punctuation, ascii_letters
 start = time()
 setrecursionlimit(10**6)
 
-def Return(count):
-    global point_key
+def CaesarCode(count):
+    global CaesarKey
     if count == 0: return 0
-    point_key += chars[randrange(0, len(chars)-1)]
+    CaesarKey += chars[randrange(0, len(chars)-1)]
     
-    Return(count-1)
+    CaesarCode(count-1)
+    return 0
+    
+def cryption(txt):
+    global point_crypKey
+    if len(txt) == 0: return 0
+    
+    point_crypKey += CaesarCodeKey[CaesarCodeChars.index(txt[0])]
+    cryption(txt[1:])
+    return 0
+
+def encryption(txt):
+    global point_encrypKey
+    if len(txt) == 0: return 0
+    
+    for idx, k in zip(CaesarCodeKey, CaesarCodeChars):
+        if idx == "".join(txt[:10001]):
+            point_encrypKey += k
+            break
+    encryption(txt[10001:])
+    return 0
     
 chars = digits + punctuation + ascii_letters
 chars = list(chars)
@@ -24,16 +44,25 @@ Key = chars.copy()
 
 shuffle(Key)
 
-CaesarCode = {}
+CaesarCodeChars = []
+CaesarCodeKey = []
 
 for char, key in zip(chars, Key):
-    point_key = key
+    CaesarKey = key
 
-    Return(10000)
-    CaesarCode[char] = point_key
+    CaesarCode(10000)
+    CaesarCodeChars.append(char)
+    CaesarCodeKey.append(CaesarKey)
 end = time()
-print(f"crypt : {CaesarCode} ---- time : {end-start:.5f}sec")
+#print(f"crypt : {CaesarCode} ---- time : {end-start:.5f}sec")
 
 
+#crypt
+text = input()
+point_crypKey = ''
+cryption(list(text))
+print(f"crypKey : {point_crypKey}")
 #encrypt
-
+point_encrypKey = ''
+encryption(list(point_crypKey))
+print(f"encrypKey : {point_encrypKey}")
